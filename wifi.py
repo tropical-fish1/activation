6764e7754964cf0c449f28248ac1380a7160a662
tropical-fish1 <tropical__fish@outlook.com>
2025-03-23 08:47:52 +0800

    Update wifi.py

--- wifi.py
+++ wifi.py
@@ -1,1 +1,6 @@
-
+import pywifi
+from pywifi import const, Profile
+import itertools
+key_n = "1234567890qazxswedcvfrtgbnhyujmkiolpQAZWSXEDCRFVTGBYHNUJMIKOLP"
+for key in itertools.permutations(key_n):
+  
